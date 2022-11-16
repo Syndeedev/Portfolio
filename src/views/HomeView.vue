74eb770c754f758109e31d947508ef14718606c2
cynthia <cynthia@seomonitor.com>
2022-11-16 15:29:30 +0100

    Install vue router and project clean up

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="justify-center flex bg-yellow-300 items-center h-screen">
+    <div class="text-4xl">Hello 👋🏼</div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomeView",
+  // setup() {},
+});
+</script>
