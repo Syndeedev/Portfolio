f5484e8772af69fdc3d4ffe1590e8a080b327e2a
cynthia <cynthia@seomonitor.com>
2022-11-16 20:36:17 +0100

    fix mobile responsiveness and improved spacing

--- src/components/LandingPageLayout.vue
+++ src/components/LandingPageLayout.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="bg-black h-screen">
+  <div class="">
     <div class="max-w-screen-xl m-auto">
-      <div class="flex flex-col md:flex-row px-4 md:px-10 lg:px-20">
-        <div class="w-1/2 text-white pt-20 max-h-screen overflow-hidden">
+      <div class="flex flex-col md:flex-row px-6 md:pr-0 md:pl-10 lg:pl-20">
+        <div
+          class="w-full md:w-1/2 text-white pt-20 md:max-h-screen md:overflow-hidden"
+        >
           <div class="md:max-w-[420px] flex flex-col">
             <span class="text-6xl font-bold">Cynthia Ineh</span>
             <span class="text-2xl font-semibold mt-4">Software Engineer</span>
@@ -15,25 +17,39 @@
               teammate. I have worked on several solutions across different
               industries giving me the experience to think far and wide.
             </span>
-            <div class="mb-4 mt-8 text-xs tracking-widest flex items-center">
+            <div
+              class="mb-4 w-fit mt-8 text-xs cursor-pointer tracking-widest text-[#949495] flex items-center hover:text-white transition-all ease-in-out duration-500 group"
+            >
               <span>01 </span>
-              <div class="w-8 h-[1px] bg-white mx-4"></div>
+              <span
+                class="w-8 h-px mx-3 bg-[#949495] group-hover:w-16 group-hover:bg-white transition-all ease-in-out duration-500"
+              ></span>
               <span>PROJECTS</span>
             </div>
-            <div class="my-4 text-xs tracking-widest flex items-center">
+            <div
+              class="my-4 w-fit text-xs cursor-pointer tracking-widest text-[#949495] flex items-center hover:text-white transition-all ease-in-out duration-500 group"
+            >
               <span>02 </span>
-              <div class="w-8 h-[1px] bg-white mx-4"></div>
+              <span
+                class="w-8 h-px mx-3 bg-[#949495] group-hover:w-16 group-hover:bg-white transition-all ease-in-out duration-500"
+              ></span>
               <span>RESUME</span>
             </div>
-            <div class="my-4 text-xs tracking-widest flex items-center">
+            <div
+              class="my-4 w-fit text-xs cursor-pointer tracking-widest text-[#949495] flex items-center hover:text-white transition-all ease-in-out duration-500 group"
+            >
               <span>03 </span>
-              <div class="w-8 h-[1px] bg-white mx-4"></div>
+              <span
+                class="w-8 h-px mx-3 bg-[#949495] group-hover:w-16 group-hover:bg-white transition-all ease-in-out duration-500"
+              ></span>
               <span>CONTACT</span>
             </div>
           </div>
         </div>
-        <div class="w-1/2 text-white max-h-screen overflow-y-scroll pt-20">
-          <span class="text-5xl">Hello</span>
+        <div
+          class="w-full md:w-1/2 text-white pt-20 md:max-h-screen md:overflow-y-scroll md:pl-10 md:pr-10 lg:pr-20"
+        >
+          <span class="text-5xl">Projects</span>
           <router-view />
           <div class="text-white pt-20">
             <div class="md:max-w-[420px] flex flex-col">
